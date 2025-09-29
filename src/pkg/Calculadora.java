a590723c63a1f53e1147bd587ed531e972ea2f00
usuario <usuario@pc21-085>
2025-09-29 16:56:19 +0200

    Programa calculadora!

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,27 @@
+package pkg;
+
+public class Calculadora {
+
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+
+	public static int resta(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiplicacion(int a, int b) {
+		return a * b;
+	}
+	
+	public static int division(int a, int b) {
+		
+		if (b == 0)
+			return -1;
+		else
+			return a / b;
+		
+		
+	}
+
+}
